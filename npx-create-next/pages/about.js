7058e016f5e6f09cc4add07aa1adcc24795f2813
ma5hr00m <3411281455@qq.com>
2023-05-15 23:25:49 +0800

    update

---
+++ npx-create-next/pages/about.js
@@ -0,0 +1,11 @@
+function About() {
+    return (
+        <img
+            src="https://i.imgur.com/MK3eW3Am.jpg"
+            alt="Katherine Johnson"
+        />
+    )
+}
+
+
+export default About
---
+++ npx-create-next/pages/profile.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <ul>
+            <li>
+                <Link href="/post/abc">
+                    <a>Go to the /post/abc page!</a>
+                </Link>
+            </li>
+        </ul>
+    )
+}
+
+export default Home
